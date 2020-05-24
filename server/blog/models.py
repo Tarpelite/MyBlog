5e9ce93f4b9b38da42adef182a0415814498e895
Tarpelite <tarpelite_chan@foxmail.com>
2020-05-24 14:20:03 +0800

    debug

--- server/blog/models.py
+++ server/blog/models.py
@@ -11,6 +11,7 @@ class Post(models.Model):
 
     title = models.CharField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
+    introduction = models.TextField(default="")
     updated_on = models.DateTimeField(auto_now= True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
